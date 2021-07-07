7d2113f7ec0eed107fb4868efc6cfffd8b8e46f7
Gopi <gopikrishna.gummadidala@gmail.com>
2021-07-07 18:42:18 +0200

    added test cases

--- EIM-assessment-main/EIM-assessment.Tests/BoardControllerTests.cs
+++ EIM-assessment-main/EIM-assessment.Tests/BoardControllerTests.cs
@@ -4,6 +4,8 @@ using EIM_assessment.Repository;
 using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EIM_assessment.Tests
 {
@@ -11,12 +13,14 @@ namespace EIM_assessment.Tests
     {
         private BoardsController controller;
         private Mock<IBoardRepository> boardRepoMock;
+        private List<Board> boardsMockData;
 
         [SetUp]
         public void Setup()
         {
             boardRepoMock = new Mock<IBoardRepository>();
             controller = new BoardsController(boardRepoMock.Object);
+            boardsMockData = new List<Board> { new Board { Id = 1, Name = "Board 1", CreatedAt = DateTime.Now }, new Board { Id = 2, Name = "Board 2", CreatedAt = DateTime.Now } };
         }
 
         [Test]
@@ -59,6 +63,26 @@ namespace EIM_assessment.Tests
             controller.Find(1);
 
             boardRepoMock.Verify(x => x.Find(1), Times.Once);
+        }
+
+        [Test]
+        public async Task Post_SaveBoards()
+        {
+            boardRepoMock.Setup(x => x.SaveBoards(It.IsAny<List<Board>>())).ReturnsAsync(true);
+
+            var res = await controller.Post(boardsMockData);
+
+            boardRepoMock.Verify(x => x.SaveBoards(boardsMockData), Times.Once);
+        }
+
+        [Test]
+        public async Task Post_SavePostToBoard()
+        {
+            boardRepoMock.Setup(x => x.SavePostToBoard(It.IsAny<PostIt>())).ReturnsAsync(true);
+            var post = new PostIt() { BoardId = 1, Id = 1, Text = "Sample post", CreatedAt = DateTime.Now };
+            var res = await controller.SavePostToBoard(post);
+
+            boardRepoMock.Verify(x => x.SavePostToBoard(post), Times.Once);
         }
     }
 }
